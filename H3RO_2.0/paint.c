9b93f2cd86f1576a057844d6231fc11731513f86
Zard0z <sylvainbarbier06@gmail.com>
2016-12-13 22:37:47 +0100

    Add files via upload

--- H3RO_2.0/paint.c
+++ H3RO_2.0/paint.c
@@ -52,26 +52,22 @@ void paint(void)
     paintObject(&player);
 
     //Affhage de tous les monstres
-    for(i = 0 ; i < game.nombreMonstres ; i++)
-    {
-        paintObject(&monster[i]);
-    }
+    for(i = 0 ; i < game.nombreMonstres ; i++) paintObject(&monster[i]);
 
-    //affichage des platefromes
+    //affichage des plateformes
     paintPlatform();
 
-    //affichages des projectiles
+    //affichage des projectiles
     paintProjectiles();
 
-    //Affichage de l'HUD (lives, etoiles etc...)
+    //Affichage du HUD (lives, etoiles, coeurs)
     paintHud();
 
-    //fonction SDL qui raffraichi l'ecran
+    //fonction SDL qui raffraichit l'ecran
     SDL_Flip(game.screen);
 
    //Délai
     SDL_Delay(1);
-
 }
 
 /**Fonction qui charge les images et les renvoies en SDL_Surface*/
@@ -81,17 +77,17 @@ SDL_Surface *loadImage(char *name)
     SDL_Surface *temp = IMG_Load(name);
     SDL_Surface *image;
 
-    //Di on arrive pas à la charger on quitte le programme et on affiche dans la commande le problème
+    //Si on n'arrive pas à la charger on quitte le programme et on affiche dans la commande le problème
     if (temp == NULL)
     {
         printf("Failed to load image %s\n", name);
         return NULL;
     }
 
-    //Ajout de la transparence RGB à l'image défini dans le def.h
-    SDL_SetColorKey(temp, (SDL_SRCCOLORKEY | SDL_RLEACCEL), SDL_MapRGB(temp->format, TRANS_R, TRANS_G,    TRANS_B));
+    //Ajout de la transparence RVB à l'image définie dans le def.h
+    SDL_SetColorKey(temp, (SDL_SRCCOLORKEY | SDL_RLEACCEL), SDL_MapRGB(temp->format, TRANS_R, TRANS_G, TRANS_B));
 
-    //Convertion au format de l'écran
+    //Conversion au format de l'écran
     image = SDL_DisplayFormat(temp);
 
     //On libere l'image temporaire
@@ -105,22 +101,14 @@ SDL_Surface *loadImage(char *name)
     return image;
 }
 
-/**Fonction de gestion du delay pour avoir 60 FPS*/
+/**Fonction de gestion du délai pour avoir 60 FPS*/
 void delay(unsigned int frameLimit)
 {
     unsigned int ticks = SDL_GetTicks();
-    if (frameLimit < ticks)
-    {
-        return;
-    }
-    if (frameLimit > ticks + 16)
-    {
-        SDL_Delay(16);
-    }
-    else
-    {
-        SDL_Delay(frameLimit - ticks);
-    }
+    if (frameLimit < ticks) return;
+
+    if (frameLimit > ticks + 16) SDL_Delay(16);
+    else SDL_Delay(frameLimit - ticks);
 }
 
 /**Fonction qui gère l'affichage de l'HUD*/
@@ -128,12 +116,13 @@ void paintHud(void)
 {
     int ysource;
     int xsource;
-    //Cré un variable de type text pour l'affichage du nombre de vie et de coeur
+
+    //Créé une variable de type text pour l'affichage du nombre de vies et de coeurs
     char text[200];
 
     int i;
     //Affiche le nombre de coeurs avec un decalage de 32 pixels
-    for( i = 0; i < player.life; i++)
+    for(i = 0; i < player.life; i++)
     {
         // Calcul pour découper le tileset comme dans la fonction paintMap()
         ysource = (TILE_POWER_UP_DEBUT + 1) / 10 * TILE_SIZE;
@@ -141,16 +130,16 @@ void paintHud(void)
         paintTile(map.tileSet, 60 + i * 32, 20, xsource, ysource);
     }
 
-    //Affichage des lives en bas à droite de l'écran
+    //Affichage des vies en bas à droite de l'écran
     paintImage(game.HUD_vie, 900, 600);
 
-    //On met le nombre de vie dans la chaine de carractere
+    //On met le nombre de coeurs dans la chaîne de caractère
     sprintf(text, "%d", game.lives);
 
-    //Puis on reutilise la fonction d'affichage de string créée précedement
+    //Puis on réutilise la fonction d'affichage de string créée précedement
     paintString(text, 970, 610, font);
 
-    //Affichage du nombre d'étoile
+    //Affichage du nombre d'étoiles
     ysource = (TILE_POWER_UP_DEBUT) / 10 * TILE_SIZE;
     xsource = (TILE_POWER_UP_DEBUT) * TILE_SIZE;
 
