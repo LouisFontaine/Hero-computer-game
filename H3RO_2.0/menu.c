5ea0d0428241603b3596fa942d25ff6a7604db9b
Zard0z <sylvainbarbier06@gmail.com>
2016-12-13 22:02:52 +0100

    Add files via upload

--- H3RO_2.0/menu.c
+++ H3RO_2.0/menu.c
@@ -1,17 +1,16 @@
 /**Fichier contenant les fonctions qui permettent d'afficher le menu de démarage du
-game.et le menu pause*/
+game et le menu pause*/
 
 #include "menu.h"
 
-/**Fonction qui va lire les imputs et qui va déplacer le curseur de start à quit en
-fonction du choix de l'utilisateur et qui va se charger de lancer le game. quand
-l'utilisateur le décide*/
+/**Fonction qui va lire les inputs et qui va déplacer le curseur de start à quit en
+fonction du choix de l'utilisateur et qui va se charger de lancer le jeu quand l'utilisateur le décide*/
 void renewStartMenu(void)
 {
     //Quand on appuie sur flêche du bas
     if(input.down == 1)
     {
-        //Si choice égual à O (start), on le met sur 1 (quit)
+        //Si choice égal à O (start), on le met sur 1 (quit)
         if(game.choice == 0) game.choice++;
         input.down = 0;
     }
@@ -19,13 +18,13 @@ void renewStartMenu(void)
     //Quand on appuie sur flêche du haut
     if(input.up == 1)
     {
-        //Si choice égual à 1 (Quit), on le met sur 0 (Start)
+        //Si choice égal à 1 (Quit), on le met sur 0 (Start)
         if(game.choice == 1) game.choice--;
         input.up = 0;
     }
 
     //Quand on appuie sur  la touche Enter et qu'on est déjà sur Start (0) alors on
-    //recharge le game.et on quitte l'état menu (game.onMenu = 0)
+    //recharge le jeu et on quitte l'état menu (game.onMenu = 0)
     if(input.enter)
     {
         if(game.choice == 0)
@@ -33,7 +32,8 @@ void renewStartMenu(void)
             player.checkpointActif = 0;
             initializePlayer();
             changeMap();
-            //On réinitialise les variables du game.pour commencer une nouvelle partie
+
+            //On réinitialise les variables du jeu pour commencer une nouvelle partie
             game.lives = 3;
             game.etoiles = 0;
             game.onMenu = 0;
@@ -47,8 +47,7 @@ void renewStartMenu(void)
 ///Fonction qui se charge de l'affichage du menu Start
 void paintStartMenu(void)
 {
-
-    //On utilise une chaine de carractère qui va contenir notre texte à afficher
+    //On utilise une chaîne de caractère qui va contenir notre texte à afficher
     char text[200];
 
     paintImage(game.fondMenuStart, 0, 0);
@@ -84,18 +83,16 @@ void paintStartMenu(void)
 
 }
 
-/**Fonction qui va lire les imputs pour pouvoir relancer le game. lorsque l'utilisateur
+/**Fonction qui va lire les inputs pour pouvoir relancer le jeu lorsque l'utilisateur
 rappuie sur enter*/
 void renewPauseMenu(void)
 {
-
     //Si l'utilisateur appuie sur Enter alors quitte l'état Onmenu
     if(input.enter)
     {
         game.onMenu = 0;
         input.enter = 0;
     }
-
 }
 
 ///Fonction qui se charge de l'affichage du menu Pause
@@ -109,27 +106,27 @@ void paintPauseMenu(void)
 
 }
 
+///Fonction qui relance le menu start à la fin du jeu
 void renewEndMenu(void)
 {
     //Si l'utilisateur appuie sur Enter alors quitte l'état On passe au menu start
     if(input.enter)
     {
-        //On ne nchange pas l'state onMenu mais on change le type de menu
+        //On ne change pas l'état onMenu mais on change le type de menu
         game.menuType = START;
         input.enter = 0;
     }
 }
 
+///Fonction qui affiche le menu de victoire
 void paintEndMenu(void)
 {
-    //On utilise une chaine de carractère qui va contenir notre texte à afficher
+    //On utilise une chaine de caractère qui va contenir notre texte à afficher
     char text[200];
 
     paintImage(game.fondMenuStart, 0, 0);
     sprintf(text, "VOUS AVEZ GAGNE !");
     paintString(text, 295, 400, font);
-    sprintf(text, "APPUYER SUR ENTREE POUR CONTINUER");
+    sprintf(text, "APPUYEZ SUR ENTREE POUR CONTINUER");
     paintString(text, 50, 600, font);
-
-
 }
