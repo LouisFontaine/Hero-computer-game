03b43dbb9bff2ba8faa60b5ba59e56e9fa82ebe5
Zard0z <sylvainbarbier06@gmail.com>
2016-12-13 17:51:12 +0100

    Add files via upload

--- H3RO_2.0_level_editor/main.h
+++ H3RO_2.0_level_editor/main.h
@@ -1,21 +1,19 @@
- #include "structs.h"
+#include "structs.h"
 
-  /* Prototypes des fonctions utilisées */
+/* Prototypes des fonctions utilisées */
 
-  extern void init(char *);
-  extern void clean(void);
-  extern void getInput(void);
-  extern void paint(void);
-  extern void load(void);
-  extern void delay(unsigned int frameLimit);
-  extern void update(void);
+extern void init(char *);
+extern void clean(void);
+extern void getInput(void);
+extern void paint(void);
+extern void load(void);
+extern void delay(unsigned int frameLimit);
+extern void update(void);
 
 
-  /* Déclaration des structures globales utilisées par le game */
+/* Déclaration des structures globales utilisées par le game */
 
-  Input input;
-  Gestion game;
-  Map map;
-
-  /* Ligne à rajouter : */
-  Cursor cursor;
+Input input;
+Gestion game;
+Map map;
+Cursor cursor;
